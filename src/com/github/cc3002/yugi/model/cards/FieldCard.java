cfbd10174ec1a47aef1b9a070debc783116f0307
SL8R <islaterm@gmail.com>
2020-10-01 22:37:46 -0300

    Basic GameMat failing test

--- src/com/github/cc3002/yugi/model/cards/FieldCard.java
+++
@@ -1,9 +0,0 @@
-package com.github.cc3002.yugi.model.cards;
-
-public class FieldCard implements ICard {
-
-  @Override
-  public void play() {
-
-  }
-}
--- src/com/github/cc3002/yugi/model/cards/IMonsterCard.java
+++
@@ -1,5 +0,0 @@
-package com.github.cc3002.yugi.model.cards;
-
-public interface IMonsterCard extends ICard {
-  void addTribute(IMonsterCard card);
-}
