85a18b3250d8746943c7e80038d67676318fbcde
SL8R <islaterm@gmail.com>
2020-10-06 15:28:48 -0300

    Cards: Implemented equals and hashCode methods

--- src/main/java/dcc/cc3002/king/Card.java
+++ src/main/java/dcc/cc3002/king/Card.java
@@ -1,5 +1,23 @@
 package dcc.cc3002.king;
 
+import java.util.Objects;
+
+/**
+ * Cards are the main element of the game.
+ * <p>
+ * A card can be magical or a monster.
+ *
+ * @author Ignacio Slater Muñoz
+ */
 public class Card {
 
+  @Override
+  public int hashCode() {
+    return Objects.hash(Card.class);
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    return obj instanceof Card;
+  }
 }
