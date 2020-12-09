0b0cd6b4e4a6fb18c61250372d75f262ccff14ed
eduardo-barria <32887365+eduardo-barria@users.noreply.github.com>
2020-12-09 12:39:24 -0300

    Update IEventHandler.java

--- src/main/java/cl/uchile/dcc/cc3002/king/controller/IEventHandler.java
+++ src/main/java/cl/uchile/dcc/cc3002/king/controller/IEventHandler.java
@@ -3,7 +3,7 @@ package cl.uchile.dcc.cc3002.king.controller;
 import java.beans.PropertyChangeListener;
 
 /*
-Esta clase la implementarán todas las clases que respondan a la jugada de una carta.
+Esta interface la implementarán todas las clases que respondan a la jugada de una carta.
 */
 
 public interface IEventHandler extends PropertyChangeListener {
